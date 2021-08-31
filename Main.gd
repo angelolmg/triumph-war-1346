4457474e286ec93e317471117f90646de40875f8
brayanleo1 <57221030+brayanleo1@users.noreply.github.com>
2021-08-31 17:24:00 -0300

    HUD with ally counter
    Funcionando, fazendo o update da quantidade de alliados(entidades dentro do grupo Allies) a cada frame(não é a melhor forma para algo que requer performance, mas...)

--- Main.gd
+++ Main.gd
@@ -7,9 +7,17 @@ onready var player: Player = $Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_update_allies()
 	randomize()
 	GlobalSignals.connect("bullet_fired", bullet_manager, "handle_bullet_spawned")
 	
+
+func _update_allies():
+	var allyNumb = get_tree().get_nodes_in_group("Allies")
+	$HUD/HBoxContainer/Bars/AlliesStats/Background/Number.text = str(allyNumb.size())
+	
+func _process(delta):#Roda todo frame
+	_update_allies()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
